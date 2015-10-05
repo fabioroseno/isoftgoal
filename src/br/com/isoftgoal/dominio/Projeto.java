eb97ab48acecb4a63465cd02e168dc34668eadbf
fabioroseno <fabiorosenos@gmail.com>
2015-10-04 22:47:38 -0300

    CRUD Projeto

--- src/br/com/isoftgoal/dominio/Projeto.java
+++ src/br/com/isoftgoal/dominio/Projeto.java
@@ -46,18 +46,86 @@ public class Projeto implements Serializable {
     @NotNull
     @Column(name="dt_cadastro")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date dtCadastro;
+    private Date dataCadastro;
 
     @Column(name="dt_inicio")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date dtInicio;
+    private Date dataInicio;
 
     @Column(name="dt_previsao_termino")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date dtPrevisaoTermino;
+    private Date dataPrevisaoTermino;
     
     @Column(name="dt_termino")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date dtTermino;
+    private Date dataTermino;
+    
+    @Lob
+    @Editor(propertyType = PropertyType.IMAGE)
+    private byte[] foto;
+
+	public Integer getCodProjeto() {
+		return codProjeto;
+	}
+
+	public void setCodProjeto(Integer codProjeto) {
+		this.codProjeto = codProjeto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataPrevisaoTermino() {
+		return dataPrevisaoTermino;
+	}
+
+	public void setDataPrevisaoTermino(Date dataPrevisaoTermino) {
+		this.dataPrevisaoTermino = dataPrevisaoTermino;
+	}
+
+	public Date getDataTermino() {
+		return dataTermino;
+	}
+
+	public void setDataTermino(Date dataTermino) {
+		this.dataTermino = dataTermino;
+	}
+
+	public byte[] getFoto() {
+		return foto;
+	}
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
 
 }
